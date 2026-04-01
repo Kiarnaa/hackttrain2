e73468f4e200a2bd7df86bb0303101eb79e22377
ANDRIANARIJERY Maharavo <toavinamaharavo@gmail.com>
2026-04-01 13:45:09 +0300

    navadika tao am controller le payement.js

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const paymentController = require("../controllers/payment");
+const paymentController = require("../controller/payment");
 
 /**
  * Initiate a payment
